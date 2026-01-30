f13d219f51f7528333ae3194ba452c2df0af6efa
SkSahilAli2006 <sksahilali0121@gmail.com>
2026-01-30 20:51:16 +0530

    Add files via upload

---
+++ QRCODE_MAKER.py
@@ -0,0 +1,10 @@
+import qrcode as qr
+img = qr.make("https://www.youtube.com/@sankii_snipper307")
+img.save("MY_GAMING-YT Channel.png")
+
+
+import qrcode as qr
+img = qr.make("https://www.instagram.com/ceogage_clips01/?__pwa=1")
+img.save("Sourav_Insta.png")
+
+
